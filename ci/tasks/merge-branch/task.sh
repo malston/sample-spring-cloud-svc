29a78510eb6063aa428b8bc976a9cceb15da8ac3
Mark Alston <malston@pivotal.io>
2017-08-25 10:43:20 -0600

    Change TARGET to SOURCE for branch name

--- ci/tasks/merge-branch/task.sh
+++ ci/tasks/merge-branch/task.sh
@@ -5,5 +5,5 @@ shopt -s dotglob
 mv -f ../to-repo/* ./
 git config --global user.email "${GIT_EMAIL}"
 git config --global user.name "${GIT_NAME}"
-git remote add -f ${TARGET_BRANCH_NAME} ../from-repo
-git merge --no-edit ${TARGET_BRANCH_NAME}/${TARGET_BRANCH_NAME}
+git remote add -f ${SOURCE_BRANCH_NAME} ../from-repo
+git merge --no-edit ${SOURCE_BRANCH_NAME}/${SOURCE_BRANCH_NAME}
