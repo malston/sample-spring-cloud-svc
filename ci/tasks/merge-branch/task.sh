fad5e8429530565b8bfa0e32bf01f173d6df752e
Mark Alston <malston@pivotal.io>
2017-08-25 11:58:57 -0600

    Merge local branch not remote

--- ci/tasks/merge-branch/task.sh
+++ ci/tasks/merge-branch/task.sh
@@ -6,4 +6,4 @@ mv -f ../to-repo/* ./
 git config --global user.email "${GIT_EMAIL}"
 git config --global user.name "${GIT_NAME}"
 git remote add -f ${SOURCE_BRANCH_NAME} ../from-repo
-git merge --no-edit ${SOURCE_BRANCH_NAME}/${SOURCE_BRANCH_NAME}
+git merge --no-edit ${SOURCE_BRANCH_NAME}
