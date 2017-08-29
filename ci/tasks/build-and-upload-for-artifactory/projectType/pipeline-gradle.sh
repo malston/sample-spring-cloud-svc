0331d24152342eef64f8046d09b69a1afdf02dcd
Mark Alston <malston@pivotal.io>
2017-08-29 12:54:37 -0600

    Hacking

---
+++ ci/tasks/build-and-upload-for-artifactory/projectType/pipeline-gradle.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+function build() {
+    echo "Additional Build Options [${BUILD_OPTIONS}]"
+
+    if [[ "${CI}" == "CONCOURSE" ]]; then
+        ./gradlew clean build deploy -PnewVersion=${PIPELINE_VERSION} -DREPO_WITH_BINARIES=${REPO_WITH_BINARIES} --stacktrace ${BUILD_OPTIONS} || ( $( printTestResults ) && return 1)
+    else
+        ./gradlew clean build deploy -PnewVersion=${PIPELINE_VERSION} -DREPO_WITH_BINARIES=${REPO_WITH_BINARIES} --stacktrace ${BUILD_OPTIONS}
+    fi
+    cp -p build/libs/*.jar ../out
+}
+
+export -f build
